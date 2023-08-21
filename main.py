bd668fb57f3b1efefffa829f2f74fb781fc49403
Abilionb <abilionbarros@gmail.com>
2023-08-21 00:16:50 -0300

    add main

---
+++ main.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Acidentes na cidade do Recife",
+    page_icon=":pickup_truck:",
+    initial_sidebar_state="expanded",
+)
+
+st.write("#Acidentes na cidade do Recife :pickup_truck: 2019-2022")
+
+st.sidebar.success("Select a demo above.")
+
+st.markdown(
+    """##### We are exploring Vehicle incidents accross england whilst utilising the capabilities of Streamlit, Snowflake, Carto and Tableau.
+    
+"""
+)
